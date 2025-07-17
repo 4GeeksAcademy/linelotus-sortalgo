16b16082699b415471db5eecfa91e77a67d30669
linelotus <189575054+linelotus@users.noreply.github.com>
2025-07-17 03:42:29 +0000

    card gen

--- src/app.js
+++ src/app.js
@@ -2,10 +2,26 @@ import "bootstrap";
 import "./style.css";
 
 
-import "./assets/img/rigo-baby.jpg";
+//import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+document.querySelector('.card').classList.add(generateRandomSymb());
+document.querySelector('.card').innerHTML = (generateRandomNumb());
+  };
+
+  let generateRandomNumb = () =>{
+   
+    let numbers = ["2","3","4","5","6","7","8","9","10","A","Q","J","K"];
+
+
+     let indexNumbers = Math.floor(Math.random() * numbers.length);
+     return numbers[indexNumbers];
+
+  };
+ let generateRandomSymb = () =>{
+  
+    let symb = ["diams","spades","hearts","clubs"];
+     let indexSymb = Math.floor(Math.random() * symb.length);
+     return symb[indexSymb];
+ };
